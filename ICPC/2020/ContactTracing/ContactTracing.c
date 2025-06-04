a3332d1d9ad3c1d40c1e4960bf09ee9dfa2388ea
Palm <241it002i@s.internet.ac.jp>
2025-06-04 23:47:32 +0900

    ICPCのB問題を実装しました。感染者の隣にいたら、もう一人にもどんどん感染させるプログラム。

---
+++ ICPC/2020/ContactTracing/ContactTracing.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int m, n, p; // *利用者の人数、記録の件数、感染した人のID
+int a[256], b[256];
+int ans[256]; // *感染した人のIDを格納する
+int infection[256]; // *感染フラグ
+
+int main()
+{
+    int i;
+    int j;
+    int cnt;
+    j = 0;
+    while(1)
+    {
+        scanf("%d %d %d", &m, &n, &p);
+        if(m == 0 && n == 0 && p == 0)
+            break;
+        i = 0;
+        while(i < n)
+        {
+            scanf("%d %d", &a[i], &b[i]);
+            i++;
+        }
+        i = 1;
+        while (i <= m)
+        {
+            infection[i] = 0;
+            i++;
+        }
+        infection[p] = 1;
+        // *直接または間接的に感染した人を記録する
+        i = 0;
+        while (i < n)
+        {
+            if (infection[a[i]] && !infection[b[i]])
+                infection[b[i]] = 1;
+            else if (infection[b[i]] && !infection[a[i]])
+                infection[a[i]] = 1;
+            i++;
+        }
+        // *感染した人のIDを格納する
+        i = 1;
+        cnt = 0;
+        while (i <= m)
+        {
+            if (infection[i])
+                cnt++;
+            i++;
+        }
+        ans[j++] = cnt;
+    }
+    i = 0;
+    while (i < j)
+    {
+        printf("%d\n", ans[i]);
+        i++;
+    }
+    return 0;
+}
