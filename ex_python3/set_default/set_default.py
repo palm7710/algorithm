f3c4b32b4ec75e57cb7ab7ad633d3ec879196917
Palm <241it002i@s.internet.ac.jp>
2025-11-03 15:41:33 +0900

    デフォルト引数

---
+++ ex_python3/set_default/set_default.py
@@ -0,0 +1,60 @@
+class Customer:
+    def __init__(self, id, age):
+        self.id = id
+        self.age = age
+        self.bill = 0
+    
+    def order_food(self, price):
+        self.bill += price
+
+    def order_softdrink(self, price):
+        self.bill += price
+
+    def order_alcohol(self, price):
+        pass
+
+class AdultCustomer(Customer):
+    def __init__(self, id, age):
+        super().__init__(id, age)
+        self.discount = False
+
+    def order_alcohol(self, price):
+        self.bill += price
+        self.discount = True
+    
+    def order_food(self, price):
+        if self.discount:
+            self.bill += max(0, price - 200)
+        else:
+            self.bill += price
+
+
+N, K = map(int, input().split())
+a = [int(input()) for _ in range(N)]
+
+customers = []
+for i in range(N):
+    if (a[i] >= 20):
+        customers.append(AdultCustomer(i + 1, a[i]))
+    else:
+        customers.append(Customer(i + 1, a[i]))
+
+for _ in range(K):
+    data = input().split()
+    n = int(data[0]) - 1
+    if data[1] == "0":
+        s, m = "alcohol", 500
+    else:
+        s, m = data[1], int(data[2])
+
+    c = customers[n]
+
+    if (s == "food"):
+        c.order_food(m)
+    elif (s == "softdrink"):
+        c.order_softdrink(m)
+    elif (s == "alcohol"):
+        c.order_alcohol(m)
+
+for c in customers:
+    print(c.bill)
