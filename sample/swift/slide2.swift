4490c3ffe297e7bcf10d1a0af8a7488196fc6b6b
Palm <241it002i@s.internet.ac.jp>
2026-02-11 01:54:02 +0900

    add slide 2

--- sample/swift/slide2.swift
+++ sample/swift/slide2.swift
@@ -2,7 +2,7 @@
 // 変えたいときは、新しいデータを作る
 struct User {
     let name: String        // let で　不変性　
-    let nickname: String?   // ? でオプショナル（NULLを許容し、クラッシュせずスキップ）
+    let nickname: String?   // ? はオプショナル（NULLを許容し、参照できる）
 
     // 自分自身は変えず、名前を変えた新しい User を返す
     func renamed(to newName: String) -> User {
@@ -13,9 +13,11 @@ struct User {
 let user = User(name: "Miki", nickname: "mikichan")
 
 // user.name は "Miki" のまま
-// 新しい User が作られる
+// 新しい User が作られる（書き換えない）
 let renamed = user.renamed(to: "Hanako")
 
 // nickname があるときだけ処理される
-// nil のときは自動的にスキップされる
+// nil のときは何もしない変換　
+// （ map は値があれば変換する / $0 はこの関数の省略された第1引数）
 let nicknameLength = renamed.nickname.map { $0.count }
+                                                                                        ッc
